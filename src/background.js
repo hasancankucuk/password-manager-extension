e771b52522e6f0951f34d0910be82f5991c9f6fa
şafak <hasancankucuk@yandex.com>
2021-08-17 02:04:32 +0300

    extension password kayit islemi

--- src/background.js
+++ src/background.js
@@ -1,20 +1,24 @@
 import localizedService from '../src/services/localized-services'
 import store from '../src/store'
+import userInfoServices from './services/userInfoServices';
+import saveAccountInfoModel from './models/saveAccountInfoModel';
 
+window.infoServices = new userInfoServices();
 window.userMail = ''
 window.userPassword = ''
 window.url = ''
 
-function createNotifications () {
-  chrome.notifications.create('NOTFICATION_ID', {
+function createNotifications(notifyId) {
+  console.log(`notifyId`, notifyId);
+  chrome.notifications.create('notifyId', {
     type: 'basic',
-    iconUrl: 'chrome-extension://abpggcnejmnbfeobnnmljaodkmkbkpcc/img/recenlty-used-icon.f28f8109.svg',
+    iconUrl: 'icons/16.png',
     title: localizedService.getLocalizedMessages('extName'),
     message: localizedService.getLocalizedMessages('notificationMessage')
   })
 }
 
-function setLocalStorage (settingsKey) {
+function setLocalStorage(settingsKey) {
   if (settingsKey.key === 'notifications') {
     localStorage.setItem('notifications', !store.state.isNotificationsEnable)
   } else if (settingsKey.key === 'toolbarNotifications') {
@@ -22,7 +26,7 @@ function setLocalStorage (settingsKey) {
   }
 }
 
-function getLocalStorage () {
+function getLocalStorage() {
   var getNotification = JSON.parse(localStorage.getItem('notification'))
   var getToolbarNotification = JSON.parse(localStorage.getItem('toolbarNotifications'))
   if (localStorage.getItem('notifications') === null && localStorage.getItem('toolbarNotifications') === null) {
@@ -47,54 +51,71 @@ function getLocalStorage () {
 //   }
 // }
 
-function getAuthStatus (message) {
+function getAuthStatus(message) {
+  window.url = "";
   if (message.popupOpen) {
     store.dispatch('setLogIn', localStorage.getItem('token'))
   }
 }
 
-function getInfo (mesg) {
+function getInfo(mesg) {
+  if (!localStorage.getItem("tokenId")) {
+    return;
+  }
+
   if (mesg.action === 'getSource') {
+    console.log(window.url === mesg.url);
+    if (window.url === mesg.url) {
+      return;
+    }
+
     var temp = document.createElement('html')
-    temp.innerHTML = mesg.source
+    temp.innerHTML = mesg.source;
     temp.getElementsByTagName('input').forEach(element => {
       if (element.type === 'text') {
         window.userMail = element.value
-      }
+      } 
+      
       if (element.type === 'password') {
         window.userPassword = element.value
       }
-      if (window.userPassword !== '' || window.userMail !== '') {
-        saveUserAccounts()
+
+      if (!!window.userPassword && !!window.userMail) {
+        window.url = mesg.url;
+        saveUserAccounts(window.userPassword, window.userMail, mesg.url);
+        window.userPassword = "";
+        window.userMail = "";
+        return;
       }
     })
   }
 }
 
-function saveUserAccounts () {
-  if (window.userPassword === '' || window.userMail === '') {
+function saveUserAccounts(userPassword, userMail, url) {
+  if (!userPassword && !userMail) {
     return;
   }
 
-  var axios = require('axios')
-  var data = JSON.stringify({ id: 0, savedUsername: window.userMail, savedPassword: window.userPassword, savedUrl: window.url })
-  var config = {
-    method: 'post',
-    url: 'https://192.168.0.28:5001/api/SaveAccountInfo',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    data: data
-  }
+  var accountInfoModel = new saveAccountInfoModel();
+  accountInfoModel.savedUrl = `${url}`;
+  accountInfoModel.savedUsername = userMail;
+  accountInfoModel.savedPassword = userPassword;
+  accountInfoModel.userInfoModelId = localStorage.getItem("tokenId");
 
-  axios(config).then(function (response) {
-  }).catch(function (error) {
-    console.log(error)
-  })
+  window.infoServices.saveAccount(accountInfoModel).then(resp => {
+    createNotifications(resp.data.id);
+    window.url = url;
+  }).catch(err => {
+    window.url = "";
+  });
 }
 
-function activatePopup () {
-  chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+function activatePopup() {
+  window.url = "";
+  chrome.tabs.query({
+    currentWindow: true,
+    active: true
+  }, function (tabs) {
     if (tabs.length > 0) {
       window.url = tabs[0].url
     } else {
@@ -102,11 +123,19 @@ function activatePopup () {
     }
   })
   try {
-    chrome.runtime.sendMessage({ msg: 'activatePopup' })
+    chrome.runtime.sendMessage({
+      msg: 'activatePopup'
+    })
   } catch (error) {
     console.log(error)
   }
 }
+
+chrome.browserAction.onClicked.addListener(function (activeTab) {
+  chrome.tabs.executeScript(null, {
+    file: "js/content-script.js"
+  });
+});
 
 // browser.tabs.onActivated.addListener(saveUserAccounts)
 browser.tabs.onActivated.addListener(getLocalStorage)
--- src/content-scripts/content-script.js
+++ src/content-scripts/content-script.js
@@ -1,3 +1,58 @@
 chrome.runtime.onMessage.addListener(function (msg) {
-  // console.log(msg)
-})
+  console.log(msg)
+});
+
+window.onload = function () {
+  setTimeout(() => {
+    var isTextFound = false;
+    document.getElementsByTagName('input').forEach(element => {
+      if (element.type === 'text') {
+        isTextFound = true;
+      } else if (isTextFound && element.type == 'password') {
+        temp = element.parentElement;
+        while (!!temp) {
+          temp = temp.parentElement;
+          if (!!temp && temp.tagName === "FORM") {
+            break;
+          }
+        }
+
+        if (!!temp) {
+          temp.addEventListener('click', function (event) {
+            browser.runtime.sendMessage({
+              action: "getSource",
+              source: document.documentElement.outerHTML,
+              url: location.href
+            });
+          }, false);
+          window.addEventListener("enter", function (event) {
+            if (event.key === 'Enter') {
+              browser.runtime.sendMessage({
+                action: "getSource",
+                source: document.documentElement.outerHTML,
+                url: location.href
+              });
+            }
+          }, false);
+        } else {
+          window.addEventListener('click', function (event) {
+            browser.runtime.sendMessage({
+              action: "getSource",
+              source: document.documentElement.outerHTML,
+              url: location.href
+            });
+          }, false);
+          window.addEventListener("enter", function (event) {
+            if (event.key === 'Enter') {
+              browser.runtime.sendMessage({
+                action: "getSource",
+                source: document.documentElement.outerHTML,
+                url: location.href
+              });
+            }
+          }, fase);
+        }
+      }
+    })
+  }, 1000);
+}
